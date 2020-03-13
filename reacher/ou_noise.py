ce7331bd537838047822b6a586325b3d5a66ae73
Deepanshu Tyagi <deepanshut041@gmail.com>
2020-03-13 23:15:11 +0530

    Added OUNOISE

---
+++ reacher/ou_noise.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+import copy
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, mu=0., theta=0.15, sigma=0.2):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        self.state = x + dx
+        return self.state
